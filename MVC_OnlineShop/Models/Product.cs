6383247a32e4198889b519604777987403f5f234
Dennis Kraus <dekraus@wesleyan.edu>
2021-01-28 12:57:00 -0500

    Feat: Adding the Product class

---
+++ MVC_OnlineShop/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_OnlineShop.Models
+{
+    public class Product
+    {
+        public string name;
+        public float price;
+        public int quantity;
+        public string image_url;
+        public string type;
+
+        public Product(string name, float price, int quantity, string image, string type)
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+            this.image_url = image;
+            this.type = type;
+        }
+    }
+}
