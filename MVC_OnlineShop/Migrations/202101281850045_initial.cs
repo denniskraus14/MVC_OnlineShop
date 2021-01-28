d199d28ebfc3ed6fff03de011afaf179262315c2
ezenity <anthonymmacallister@gmail.com>
2021-01-28 13:53:03 -0500

    Feat: Add on migration initial file
    This will allow us to create a database and its tables.

---
+++ MVC_OnlineShop/Migrations/202101281850045_initial.cs
@@ -0,0 +1,58 @@
+﻿namespace MVC_OnlineShop.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false),
+                        UserName = c.String(nullable: false),
+                        Password = c.String(nullable: false),
+                        ConfirmPassword = c.String(nullable: false),
+                        Email = c.String(nullable: false),
+                        CreatedDate = c.DateTime(nullable: false),
+                        LastLoginDate = c.DateTime(),
+                        RememberMe = c.Boolean(nullable: false),
+                        RoleId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Price = c.String(),
+                        Quantity = c.String(),
+                        Image_Url = c.String(),
+                        Type = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Roles",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Roles");
+            DropTable("dbo.Products");
+            DropTable("dbo.Customers");
+        }
+    }
+}
