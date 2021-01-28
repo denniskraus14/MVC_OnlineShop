55206d699baee129b8c4e18646edacf089833d40
Emily Hall <emily.hall@capgemini.com>
2021-01-28 12:44:16 -0600

    feat: Skeleton of Cart and Bill classes

---
+++ MVC_OnlineShop/Models/Bill.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_OnlineShop.Models
+{
+    public class Bill
+    {
+        public bool Paid {get; set;}
+        public void getTotal(Cart cart)
+        {
+            foreach (var p in cart.Products)
+            {
+                // TO DO
+            }
+        }
+
+        public override string ToString()
+        {
+            return "TO DO";
+        }
+    }
+}
---
+++ MVC_OnlineShop/Models/Cart.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVC_OnlineShop.Models.Exceptions;
+
+namespace MVC_OnlineShop.Models
+{
+    public class Cart
+    {
+        public List<Product> Products
+        {
+            get
+            { 
+                return Products;
+            }
+        }
+
+        public void Add(Product product)
+        {
+            Products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            try
+            {
+                Products.Remove(product);
+            }
+            catch
+            {
+                throw new ProductNotFoundException();
+            }
+        }
+    }
+}
+
+// To Be Deleted....
+public class Product { }
